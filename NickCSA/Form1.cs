e57af5e73b1a59f2a2a174fa017049d2b5c4ad05
nick-burgess13 <nick.burgess1114@gmail.com>
2019-12-26 18:28:41 -0500

    Things!

--- NickCSA/Form1.cs
+++ NickCSA/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +21,19 @@ namespace NickCSA
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            //string desPath = "C:\Users\Nick\Downloads";
+          
+            WebClient wc = new WebClient();
+            //wc.Headers.Add()
+            if (!Directory.Exists("D:\\") {
+                Directory.CreateDirectory("")
+            }
+            wc.DownloadFile(new Uri("https://www.kauailabs.com/public_files/navx-mxp/navx-mxp.zip"), "D:\\");
 
+            
         }
+      
+
     }
+
 }
